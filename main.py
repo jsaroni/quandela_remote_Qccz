2f526c1d919057624bc9cd3d95043e25f9574cf1
Jason Saroni <77505813+jsaroni@users.noreply.github.com>
2024-02-04 07:32:22 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+#This is an example of the file you must have in your main git branch
+import perceval as pcvl
+import math
+import numpy as np
+from perceval.utils import Encoding, PostSelect, Matrix
+from perceval.components import Circuit, Port, Unitary
+
+def get_CCZ() -> pcvl.Processor:
+
+    processor = pcvl.Processor("MPS", 6)
+    T = Matrix( [[1,0], [0, np.exp(1j*np.pi/4)]] )
+    Tdg = Matrix( [[1,0], [0, np.exp(-1j*np.pi/4)]] )
+    processor.add((2,3,4,5), pcvl.catalog["klm cnot"].build_processor())
+    processor.add((4,5), Unitary(Tdg))
+    processor.add((0,1,4,5), pcvl.catalog["klm cnot"].build_processor())
+    processor.add((4,5), Unitary(T))
+    processor.add((2,3,4,5), pcvl.catalog["klm cnot"].build_processor())
+    processor.add((4,5), Unitary(Tdg))
+    processor.add((0,1,4,5), pcvl.catalog["klm cnot"].build_processor())
+    processor.add((4,5), Unitary(T))
+    processor.add((0,1), Unitary(T))
+    processor.add((0,1,2,3), pcvl.catalog["klm cnot"].build_processor())
+    processor.add((0,1), Unitary(T))
+    processor.add((2,3), Unitary(Tdg))
+    processor.add((0,1,2,3), pcvl.catalog["klm cnot"].build_processor())
+
+    return processor
+
+print(get_CCZ())
